5928a1f385a167e5faba9d66dcd1f966b887e331
0xfedfedfe <70035962+0xfedfedfe@users.noreply.github.com>
2023-07-14 22:59:51 +0100

    Latest shader source

--- source/omaha/rasterizer/hlsl/explicit/copy_surface.hlsl
+++ source/omaha/rasterizer/hlsl/explicit/copy_surface.hlsl
@@ -7,6 +7,8 @@
 #include "shared\utilities.fx"
 #include "postprocess\postprocess.fx"
 //@generate screen
+//@entry default
+//@entry depth_to_rgba_pack
 
 LOCAL_SAMPLER_2D(source_sampler, 0);
 
@@ -15,4 +17,25 @@ float4 default_ps(screen_output IN) : SV_Target
  	float4 color= sample2D(source_sampler, IN.texcoord);
  	color*= IN.color;
  	return color*scale;
+}
+
+screen_output depth_to_rgba_pack_vs(in vertex_type IN)
+{
+	return default_vs(IN);
+}
+
+// to be able copy D32_FLOAT surface into A8R8G8B8 surface
+float4 depth_to_rgba_pack_ps(screen_output IN) : SV_Target
+{
+	// I expect to sample here the viewport space hyperbolic depth
+	// then expect a value in the [0..1) range
+	float depth = sample2D(source_sampler, IN.texcoord).r;
+
+	float4 encoded = float4(1.f, 255.f, 65025.f, 16581375.f) * depth;
+
+	encoded = frac(encoded);
+
+	encoded -= encoded.yzww * float4(1.f / 255.f, 1.f / 255.f, 1.f / 255.f, 0.f);
+
+	return encoded;
 }
--- source/omaha/rasterizer/hlsl/postprocess/downsample_4x4_block.hlsl
+++ source/omaha/rasterizer/hlsl/postprocess/downsample_4x4_block.hlsl
@@ -5,6 +5,8 @@
 #include "shared\utilities.fx"
 #include "postprocess\postprocess.fx"
 //@generate screen
+//@entry default
+//@entry linear_depth_downsample
 
 LOCAL_SAMPLER_2D(source_sampler, 0);
 
@@ -20,4 +22,21 @@ float4 default_ps(screen_output IN) : SV_Target
 	color= color / 4.0f;
 
 	return color;
+}
+
+screen_output linear_depth_downsample_vs(in vertex_type IN)
+{
+	return default_vs(IN);
+}
+
+float4 linear_depth_downsample_ps(screen_output IN) : SV_Target
+{
+	float4 depth= 0.f;
+
+	depth = tex2D_offset(source_sampler, IN.texcoord, -1, -1);
+	depth = min(depth, tex2D_offset(source_sampler, IN.texcoord, +1, -1));
+	depth = min(depth, tex2D_offset(source_sampler, IN.texcoord, -1, +1));
+	depth = min(depth, tex2D_offset(source_sampler, IN.texcoord, +1, +1));
+
+	return depth;
 }
